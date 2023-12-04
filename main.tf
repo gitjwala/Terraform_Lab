5a1734591d86c4f3a7d32a37468f1e44c460f793
gitjwala <151565079+gitjwala@users.noreply.github.com>
2023-12-04 16:17:46 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
+provider "aws" {
+  region = "us-east-2"
+}
+resource "aws_instance" "my_instance"{
+    ami = "ami-089c26792dcb1fbd4"
+    instance_type = "t2.micro"
 
+    tags = {
+        Name = "my-first-ec2"
+    }
+}
